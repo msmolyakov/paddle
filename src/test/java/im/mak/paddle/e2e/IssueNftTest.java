7b1c924fd7ed6cf65949417a317786e4d5a6ab45
Maxim Smolyakov <smolyakovmaxim@gmail.com>
2019-07-16 15:37:17 +0300

    complete Nft test

--- src/test/java/im/mak/paddle/e2e/IssueNftTest.java
+++ src/test/java/im/mak/paddle/e2e/IssueNftTest.java
@@ -17,13 +17,14 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class IssueNftTest {
 
     private Node node;
-    private Account alice;
+    private Account alice, bob;
 
     @BeforeAll
     void before() {
         node = runDockerNode(Version.MAINNET);
 
-        alice = new Account(node, 1_00000000L);
+        alice = new Account(node, 10_00000000L);
+        bob = new Account(node);
     }
 
     @AfterAll
@@ -35,12 +36,16 @@ class IssueNftTest {
     void canIssueNft() {
         long initBalance = alice.balance();
 
-        String nftId = alice.issuesNft(i -> i.name("NFT").description("My first NFT").script("true"))
+        String nftId = alice.issuesNft(i -> i.name("My NFT").description("My first NFT").script("true"))
                 .getId().toString();
 
+        alice.transfers(t -> t.to(bob).amount(1).asset(nftId));
+
         assertAll("balances",
-                () -> assertThat(alice.balance()).isEqualTo(initBalance - MIN_FEE),
-                () -> assertThat(alice.balance(nftId)).isEqualTo(1)
+                () -> assertThat(alice.balance()).isEqualTo(initBalance - MIN_FEE * 6),
+                () -> assertThat(alice.balance(nftId)).isEqualTo(0),
+
+                () -> assertThat(bob.balance(nftId)).isEqualTo(1)
                 //TODO check API nft by address
         );
     }
