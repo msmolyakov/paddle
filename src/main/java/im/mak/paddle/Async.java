01857fa2abdfdc3d0bac8d6f3e70767d29091b71
Maxim Smolyakov <smolyakovmaxim@gmail.com>
2019-08-15 23:28:01 +0300

    #2 ability to do actions asynchronously

--- src/main/java/im/mak/paddle/Async.java
+++ src/main/java/im/mak/paddle/Async.java
@@ -1,11 +1,19 @@
 package im.mak.paddle;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
 public class Async {
 
     public static void async(Runnable... actions) {
-        for (Runnable action : actions) {
-            action.run();
-        }
+        CompletableFuture[] futures = new CompletableFuture[actions.length];
+
+        IntStream.range(0, actions.length)
+                .forEach(i ->
+                        futures[i] = CompletableFuture.runAsync(actions[i])
+                );
+
+        CompletableFuture.allOf(futures).join();
     }
 
 }
---
+++ src/test/java/im/mak/paddle/AsyncTest.java
@@ -0,0 +1,30 @@
+package im.mak.paddle;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AsyncTest {
+
+    @Test
+    void asyncRan() {
+        List<String> results = new ArrayList<>();
+
+        Async.async(
+                () -> {
+                    try { Thread.sleep(10); } catch (InterruptedException ignored) {}
+                    results.add("Second!");
+                },
+                () -> {
+                    try { Thread.sleep(5); } catch (InterruptedException ignored) {}
+                    results.add("First!");
+                }
+        );
+
+        assertThat(results).containsExactly("First!", "Second!");
+    }
+
+}
